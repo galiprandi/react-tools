a46fbf80bfdbc3d3aae3be7a4487f8ee3e8b313a
Germán Aliprandi <german@egg.live>
2024-05-11 18:42:28 -0300

    Use our useDebounce implementation

--- lib/components/Input/index.tsx
+++ lib/components/Input/index.tsx
@@ -1,58 +1,58 @@
-import { ReactNode, useEffect, useState } from "react";
-import { useDebounce } from "use-debounce";
-import { valueTransforms } from "../../utilities/strings";
+import { ReactNode, useEffect, useState } from 'react'
+import { useDebounce } from '../../hooks/useDebounce'
+import { valueTransforms } from '../../utilities/strings'
 
 export function Input(props: InputProps) {
-  const {
-    label,
-    onChangeValue,
-    onChangeDebounce,
-    debounceDelay = 0,
-    transform,
-    transformFn,
-    ...restProps
-  } = props;
-  const [value, setValue] = useState(props.value ?? "");
-  const [valueDebounce] = useDebounce(value, debounceDelay);
+    const {
+        label,
+        onChangeValue,
+        onChangeDebounce,
+        debounceDelay = 0,
+        transform,
+        transformFn,
+        ...restProps
+    } = props
+    const [value, setValue] = useState(props.value ?? '')
+    const valueDebounce = useDebounce(value, debounceDelay)
 
-  // Update the debounced value
-  useEffect(() => {
-    if (onChangeDebounce) onChangeDebounce(valueDebounce);
-  }, [valueDebounce, onChangeDebounce]);
+    // Update the debounced value
+    useEffect(() => {
+        if (onChangeDebounce) onChangeDebounce(valueDebounce)
+    }, [valueDebounce, onChangeDebounce])
 
-  // Update the value
-  useEffect(() => {
-    if (onChangeValue) onChangeValue(value);
-  }, [value, onChangeValue]);
+    // Update the value
+    useEffect(() => {
+        if (onChangeValue) onChangeValue(value)
+    }, [value, onChangeValue])
 
-  // Handle the input change
-  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    let inputValue = event.target.value;
-    if (transform) inputValue = valueTransforms(inputValue, transform);
-    if (transformFn) inputValue = transformFn(inputValue);
-    if (props.onChange) props.onChange(event);
-    setValue(inputValue);
-  };
+    // Handle the input change
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        let inputValue = event.target.value
+        if (transform) inputValue = valueTransforms(inputValue, transform)
+        if (transformFn) inputValue = transformFn(inputValue)
+        if (props.onChange) props.onChange(event)
+        setValue(inputValue)
+    }
 
-  if (label)
-    return (
-      <label className={props.className}>
-        {label}
-        <input {...restProps} value={value} onChange={handleOnChange} />
-      </label>
-    );
+    if (label)
+        return (
+            <label className={props.className}>
+                {label}
+                <input {...restProps} value={value} onChange={handleOnChange} />
+            </label>
+        )
 
-  return <input {...restProps} value={value} onChange={handleOnChange} />;
+    return <input {...restProps} value={value} onChange={handleOnChange} />
 }
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
-  label?: ReactNode;
-  onChangeValue?: (value: TData) => void;
-  onChangeDebounce?: (value: TData) => void;
-  debounceDelay?: number;
-  transform?: Parameters<typeof valueTransforms>[1];
-  transformFn?: (value: string) => string;
+    extends React.InputHTMLAttributes<HTMLInputElement> {
+    label?: ReactNode
+    onChangeValue?: (value: TData) => void
+    onChangeDebounce?: (value: TData) => void
+    debounceDelay?: number
+    transform?: Parameters<typeof valueTransforms>[1]
+    transformFn?: (value: string) => string
 }
 
-type TData = React.InputHTMLAttributes<HTMLInputElement>["value"];
+type TData = React.InputHTMLAttributes<HTMLInputElement>['value']
---
+++ lib/hooks/useDebounce.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react'
+
+export function useDebounce<T>(value: T, delay: number) {
+    const [debouncedValue, setDebouncedValue] = useState<T>(value)
+
+    useEffect(() => {
+        if (!delay) return setDebouncedValue(value)
+
+        const handler = setTimeout(() => {
+            setDebouncedValue(value)
+        }, delay)
+
+        return () => {
+            clearTimeout(handler)
+        }
+    }, [value, delay])
+
+    return debouncedValue
+}
