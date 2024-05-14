004f2885e1d36b197026168ad6bcdeabfb427fbc
Germán Aliprandi <german@egg.live>
2024-05-14 13:46:17 -0300

    chore: Update App component to include DateTimeExample

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import "./App.css";
-import { InputExample } from "./Examples/InputExample";
+import './App.css'
+import { DateTimeExample } from './Examples/DateTimeExample'
+import { InputExample } from './Examples/InputExample'
 
 function App() {
-  return (
-    <main>
-      <h1>react-tools</h1>
-      <p>
-        Welcome to `react-tools`, a set of simple and intuitive utilities for
-        developing React applications. This package includes key tools that can
-        streamline your development process.
-      </p>
+    return (
+        <main>
+            <h1>react-tools</h1>
+            <p>
+                Welcome to `react-tools`, a set of simple and intuitive
+                utilities for developing React applications. This package
+                includes key tools that can streamline your development process.
+            </p>
 
-      <InputExample />
-    </main>
-  );
+            <InputExample />
+            <DateTimeExample />
+        </main>
+    )
 }
 
-export default App;
+export default App
