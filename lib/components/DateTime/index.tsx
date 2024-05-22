8308f042219ef22bb5785f99e53d35f85963ae9c
Germán Aliprandi <german@egg.live>
2024-05-21 21:30:48 -0300

    chore: Refactor DateTime component to use RFC 3339 format for date

--- lib/components/DateTime/index.tsx
+++ lib/components/DateTime/index.tsx
@@ -1,7 +1,12 @@
 import { ComponentProps, useEffect, useState } from 'react'
 import { Input } from '../../main'
 
-export const DateTime = (props: DateTimeProps) => {
+/**
+ * DateTime component for selecting date and time in RFC 3339 format.
+ * @param {DateTimeProps} props - Props for the DateTime component.
+ * @returns {JSX.Element} - DateTime component JSX.
+ */
+export const DateTime = (props: DateTimeProps): JSX.Element => {
     const { isoValue, onChangeISOValue, ...restProps } = props
     const [isoDateTime, setIsoDateTime] =
         useState<DateTimeProps['isoValue']>(isoValue)
@@ -33,7 +38,9 @@ export interface DateTimeProps
 
 // Input:  2024-05-14T13:00:00.000Z
 // Output: 2024-05-14T10:00
-const isoToDateTime = (date?: DateTimeProps['isoValue']) => {
+const isoToDateTime = (
+    date?: DateTimeProps['isoValue'],
+): string | undefined => {
     if (!date || typeof date !== 'string') return date
     const d = new Date(date)
     const local = new Date(d.getTime() - d.getTimezoneOffset() * 60000)
