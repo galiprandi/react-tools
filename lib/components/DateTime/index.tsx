8421f8a4273d6c98c072992a8149a0123ef30926
Germán Aliprandi <german@egg.live>
2024-05-22 21:39:35 -0300

    chore: Improve JSDoc

--- lib/components/DateTime/index.tsx
+++ lib/components/DateTime/index.tsx
@@ -2,9 +2,31 @@ import { ComponentProps, useEffect, useState } from 'react'
 import { Input } from '../../main'
 
 /**
- * DateTime component for selecting date and time in RFC 3339 format.
- * @param {DateTimeProps} props - Props for the DateTime component.
- * @returns {JSX.Element} - DateTime component JSX.
+ * DateTime component - A wrapper for the Input component that handles ISO date-time values.
+ *
+ * @param {DateTimeProps} props - The props for the DateTime component.
+ * @returns {JSX.Element} - Returns a JSX element representing the date-time input component.
+ *
+ * Example usage:
+ *
+ * ```tsx
+ * const MyComponent = () => {
+ *     const [isoValue, setIsoValue] = useState('2024-05-14T13:00:00.000Z');
+ *
+ *     const handleChangeISOValue = (value: string) => {
+ *         setIsoValue(value);
+ *     };
+ *
+ *     return (
+ *         <DateTime
+ *             isoValue={isoValue}
+ *             onChangeISOValue={handleChangeISOValue}
+ *             label="Select Date and Time"
+ *             className="datetime-input"
+ *         />
+ *     );
+ * };
+ * ```
  */
 export const DateTime = (props: DateTimeProps): JSX.Element => {
     const { isoValue, onChangeISOValue, ...restProps } = props
@@ -20,7 +42,7 @@ export const DateTime = (props: DateTimeProps): JSX.Element => {
         <Input
             {...restProps}
             type="datetime-local"
-            value={isoDateTime ? isoToDateTime(isoDateTime) : props.value}
+            value={isoDateTime ? iso2LocalDateTime(isoDateTime) : props.value}
             onChangeValue={(value) =>
                 typeof value === 'string'
                     ? setIsoDateTime(value)
@@ -30,15 +52,45 @@ export const DateTime = (props: DateTimeProps): JSX.Element => {
     )
 }
 
+/**
+ * The properties for the DateTime component.
+ *
+ * @interface DateTimeProps
+ * @extends {Omit<ComponentProps<typeof Input>, 'type'>}
+ *
+ * @property {string} [isoValue] - The ISO date-time string value.
+ * @property {(value: string) => void} [onChangeISOValue] - Callback function triggered when the ISO date-time value changes.
+ */
 export interface DateTimeProps
     extends Omit<ComponentProps<typeof Input>, 'type'> {
+    /**
+     * The ISO date-time string value in format "YYYY-MM-DDTHH:MM:SS.sssZ".
+     */
     isoValue?: string
+    /**
+     * Callback function triggered when component date-time value changes.
+     *
+     * @param {string} value - The new ISO date-time value in format "YYYY-MM-DDTHH:MM:SS.sssZ".
+     */
     onChangeISOValue?: (value: string) => void
 }
 
-// Input:  2024-05-14T13:00:00.000Z
-// Output: 2024-05-14T10:00
-const isoToDateTime = (
+/**
+ * Converts an ISO date-time string to a local date-time string.
+ *
+ * @param {string} [date] - The ISO date-time string.
+ * @returns {string | undefined} - The local date-time string or undefined if the input is invalid.
+ *
+ * Example:
+ *
+ * ```js
+ * // Input:  2024-05-14T13:00:00.000Z
+ * // Output: 2024-05-14T10:00
+ * const localDateTime = iso2LocalDateTime('2024-05-14T13:00:00.000Z');
+ * console.log(localDateTime); // "2024-05-14T10:00"
+ * ```
+ */
+const iso2LocalDateTime = (
     date?: DateTimeProps['isoValue'],
 ): string | undefined => {
     if (!date || typeof date !== 'string') return date
