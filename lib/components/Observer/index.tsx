6d41e58c1146bf296be9df915d719dae922a5dd6
Germán Aliprandi <german@egg.live>
2024-05-22 21:43:43 -0300

    chore: Improve JSDoc of component

--- lib/components/Observer/index.tsx
+++ lib/components/Observer/index.tsx
@@ -6,6 +6,32 @@ import {
     useRef,
 } from 'react'
 
+/**
+ * Observer component - A component's wrapper that uses the Intersection Observer API to detect when its children appear or disappear in the viewport and triggers the specified callback functions.
+ *
+ * @param {ObserverProps} props - The props for the Observer component.
+ * @returns {ObserverReturn} - Returns a React element with the specified wrapper.
+ *
+ * Example usage:
+ *
+ * ```tsx
+ * const MyComponent = () => {
+ *     const handleAppear = (entry: IntersectionObserverEntry) => {
+ *         console.log('Element appeared:', entry);
+ *     };
+ *
+ *     const handleDisappear = (entry: IntersectionObserverEntry) => {
+ *         console.log('Element disappeared:', entry);
+ *     };
+ *
+ *     return (
+ *         <Observer onAppear={handleAppear} onDisappear={handleDisappear} wrapper="section">
+ *             <p>Content to observe</p>
+ *         </Observer>
+ *     );
+ * };
+ * ```
+ */
 export const Observer = (props: ObserverProps): ObserverReturn => {
     const {
         wrapper = 'div',
@@ -33,14 +59,43 @@ export const Observer = (props: ObserverProps): ObserverReturn => {
     return createElement(wrapper, { ref, children })
 }
 
+/**
+ * The properties for the Observer component.
+ *
+ * @interface ObserverProps
+ * @extends {PropsWithChildren}
+ * @extends {IntersectionObserverInit}
+ *
+ * @property {keyof ReactHTML} [wrapper='div'] - The HTML element to wrap the children with.
+ * @property {(entry: IntersectionObserverEntry) => void} [onAppear] - Callback function triggered when the element appears in the viewport.
+ * @property {(entry: IntersectionObserverEntry) => void} [onDisappear] - Callback function triggered when the element disappears from the viewport.
+ */
 export interface ObserverProps
     extends PropsWithChildren,
         IntersectionObserverInit {
+    /**
+     * The HTML element to wrap the children with.
+     *
+     * @default 'div'
+     */
     wrapper?: keyof ReactHTML
+    /**
+     * Callback function triggered when the element appears in the viewport.
+     *
+     * @param {IntersectionObserverEntry} entry - The intersection observer entry.
+     */
     onAppear?: (entry: IntersectionObserverEntry) => void
+    /**
+     * Callback function triggered when the element disappears from the viewport.
+     *
+     * @param {IntersectionObserverEntry} entry - The intersection observer entry.
+     */
     onDisappear?: (entry: IntersectionObserverEntry) => void
 }
 
+/**
+ * The return type for the Observer component.
+ */
 type ObserverReturn = React.DetailedReactHTMLElement<
     {
         ref: React.RefObject<HTMLDivElement>
